15ac73ee475914ca364723f89f93de1f8eb3a861
Tomek2012 <Tomek2012@users.noreply.github.com>
2023-01-24 00:37:47 +0100

    Initialer Start mit Auth0 inklusive Login/Logout-Button

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { AuthModule } from '@auth0/auth0-angular';
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { TimetrackingComponent } from './pages/timetracking/timetracking.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, TimetrackingComponent, HomeComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+
+    AuthModule.forRoot({
+      domain: 'dev-5qxndj1r1v7v2wgq.us.auth0.com',
+      clientId: 'qy9vFiTzOAZOrnWc2n8jyKAsWs0KluJ8',
+      authorizationParams: {
+        redirect_uri: window.location.origin,
+      },
+    }),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
